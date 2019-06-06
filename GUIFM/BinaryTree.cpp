9efd466be8f97d56cb70ca2b48562d5eb9628788
unknown <erick.e.carrasco88@gmail.com>
2019-06-05 22:03:21 -0600

    Pulling data

--- GUIFM/BinaryTree.cpp
+++ GUIFM/BinaryTree.cpp
@@ -1,6 +1,58 @@
 #include "BinaryTree.h"
 
-BinaryTree::BinaryTree()
-{
+BinaryTree::BinaryTree(){
 
 }
+
+BinaryTree::BinaryTree(int nT){
+    root = new BinaryTreeNode(nT);
+    T= nT;
+    t=(nT-1)/2;
+
+}
+
+BinaryTreeNode* BinaryTree::getRoot(){
+    return root;
+}
+
+void BinaryTree::sort(BinaryTreeNode* node){
+    List<Key> temp;
+    for (int i = 1; i<= node->getKeys()->size;i++) {
+        temp.insert(*node->getKeyAt(i));
+    }
+    temp.sort();
+    node->getKeys()->clear();
+    for (int i=1;i<=temp.size;i++) {
+        node->getKeys()->insert(temp.getPointer(i));
+    }
+}
+
+void BinaryTree::insert(Key* k){
+    BinaryTreeNode* temp = new BinaryTreeNode();
+    temp = root;
+    temp = getLeaf(temp, k->getKey());
+    temp->getKeys()->insert(k);
+    sort(temp);
+    if(temp->getKeys()->size > temp->getT()-1){
+        //split
+    }
+}
+
+BinaryTreeNode* BinaryTree::getLeaf(BinaryTreeNode* nNode, string key){
+    bool verify = false;
+    if(!nNode->isLeaf()){
+        for(int i =1; nNode->getKeys()->size;i++){
+            if(key < nNode->getKeyAt(i)->getKey() && !verify){
+                nNode = nNode->getChildrenAt(i);
+                verify=true;
+            }
+        }
+        if(!verify){
+            nNode= nNode->getChildrenAt(nNode->getChildren()->size);
+        }
+        nNode = getLeaf(nNode, key);
+    }
+    return nNode;
+}
+
+
--- GUIFM/BinaryTree.h
+++ GUIFM/BinaryTree.h
@@ -12,6 +12,17 @@ private:
 
 public:
     BinaryTree();
+    BinaryTree(int);
+    BinaryTreeNode* getRoot();
+    void sort(BinaryTreeNode*);
+    void insert(Key*);
+    void split(BinaryTreeNode*);
+    void remove(string);
+    void merge(BinaryTreeNode*);
+    BinaryTreeNode* getLeaf(BinaryTreeNode*, string);
+    int findIndex(BinaryTreeNode*, string);
+    BinaryTreeNode* findNode(BinaryTreeNode*, string);
+
 };
 
 #endif // BINARYTREE_H
